61878ebcf73964d81787708836d9d7035a480971
ashadhyani <asha3451@hotmail.com>
2022-04-09 14:37:06 +0100

    add Constructor

--- employee.py
+++ employee.py
@@ -1,5 +1,15 @@
  class employee:
-     def__init__(self):
-    def computePayment(self):
+     def__init__(self,StaffID,LastName,FirstName,RegHours,HourlyRate,OTMultiple,TaxCredit,StandardBand):
+         self.StaffID=StaffID
+         self.LastName=LastName
+         self.FirstName=FirstName
+         self.RegHours=RegHours
+         self.HourlyRate=HourlyRate
+         self.OTMultiple=OTMultiple
+         self.TaxCredit=TaxCredit
+         self.StandardBand=StandardBand
+         def computePayment(self):
+             print("There are %d employees")
+    
         
         
