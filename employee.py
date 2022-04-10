09cea1d35f7101c3f4fbe4272ba63289d8f45e73
ashadhyani <asha3451@hotmail.com>
2022-04-11 00:55:59 +0100

    add comments

--- employee.py
+++ employee.py
@@ -8,23 +8,23 @@ class Employee:
             self.OTMultiple=OTMultiple
             self.TaxCredit=TaxCredit
             self.StandardBand=StandardBand
-        # create function for create a compute payment
+        # create function for compute payment
         def computePayment(self, count_no, date_value):
-            over_time_rate=self.HourlyRate * self.OTMultiple
-            Regular_Pay=self.RegHours * self.HourlyRate
-            Overtime_Pay=(count_no-self.RegHours)*over_time_rate
-            Gross_Pay=Regular_Pay+Overtime_Pay
+            over_time_rate=self.HourlyRate * self.OTMultiple #Calculate over time rate
+            Regular_Pay=self.RegHours * self.HourlyRate      #Calculate Regular pay 
+            Overtime_Pay=(count_no-self.RegHours)*over_time_rate #Calculate overtime pay
+            Gross_Pay=Regular_Pay+Overtime_Pay   #Calculate Gross pay
             Standard_Rate_Pay=self.StandardBand
-            Higher_Rate_Pay=Gross_Pay-Standard_Rate_Pay
+            Higher_Rate_Pay=Gross_Pay-Standard_Rate_Pay   #Calculate Higher rate pay
             Higher_Tax=0
             std_rate=round(Gross_Pay*20/100)
             if Gross_Pay>self.StandardBand:
-                Higher_Tax=(Higher_Rate_Pay*40)/100
+                Higher_Tax=(Higher_Rate_Pay*40)/100   
             else:
                 Higher_Tax=(Higher_Rate_Pay*20)/100
-            prsi=(Gross_Pay*4.0)/100
-            total_txt=std_rate+Higher_Tax
-            Net_Tax=(std_rate-self.TaxCredit)+Higher_Tax
+            prsi=(Gross_Pay*4.0)/100     #Calculate PRSI
+            total_txt=std_rate+Higher_Tax   #Calculate Total Tax
+            Net_Tax=(std_rate-self.TaxCredit)+Higher_Tax  #calculate Net Tax
             Net_Deductions=prsi+Net_Tax
             Net_Deductions=Gross_Pay-Net_Deductions
             return_val={
